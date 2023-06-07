0963e015281a2122af55a8f4e0f4b52bae5afc59
JavierGonzalez <ochentainueve@gmail.com>
2023-06-07 17:24:13 +0200

    ejemplos con logs

--- build/index.js
+++ build/index.js
@@ -4,4 +4,23 @@ console.log("hola");
 console.log("hola");
 console.log("hola");
 console.log("hola");
+const alumnos = [{
+        nombre: "Javi",
+        edad: 20,
+    },
+    {
+        nombre: "Aitor",
+        edad: 30,
+    },
+    {
+        nombre: "Alvar",
+        edad: 18
+    }];
+//console.log(alumnos)
+console.table(alumnos);
+console.time("timer");
+for (let i = 0; i < 1234567890; i++) {
+    let a = 0;
+}
+console.timeEnd("timer");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
 console.log("hola")
 
 console.log("hola")
+
 console.log("hola")
+
 console.log("hola")
+
 console.log("hola")
+
+
+interface Alumno {
+    nombre: string,
+    edad: number
+}
+
+const alumnos: Alumno[] =[{
+    nombre: "Javi",
+    edad: 20,
+},
+{
+    nombre: "Aitor",
+    edad: 30,
+},
+{
+    nombre: "Alvar",
+    edad: 18
+}]
+//console.log(alumnos)
+console.table(alumnos)
+
+console.time("timer")
+
+for(let i=0; i< 1234567; i++)
+{
+    let a =0
+}
+console.timeEnd("timer")
